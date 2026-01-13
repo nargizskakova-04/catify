be228592d7b74297c8eb3a3da16b74ee072303da
Nargiz <skakovasaule7@gmail.com>
2026-01-13 23:44:03 +0500

    feat: repository

---
+++ internal/repository/postgres/db.go
@@ -0,0 +1,36 @@
+package postgres
+
+import (
+	"catify/internal/config"
+	"database/sql"
+	"errors"
+	"fmt"
+)
+
+func NewDbConnInstance(cfg *config.Repository) (*sql.DB, error) {
+	if cfg == nil {
+		return nil, errors.New("Postgres configuration is nil")
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		cfg.DBHost,
+		cfg.DBPort,
+		cfg.DBUsername,
+		cfg.DBPassword,
+		cfg.DBName,
+		cfg.DBSSLMode,
+	)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to open database: %w", err)
+	}
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("Failed to ping database: %w", err)
+	}
+
+	db.SetMaxOpenConns(cfg.MaxConn)
+	db.SetMaxIdleConns(cfg.MaxIdleConn)
+	return db, nil
+}
